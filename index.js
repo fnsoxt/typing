bd9cf0b9ddcc9f06732d8cd332b7f1fb5e7e3c18
fnsoxt <fnsoxt@gmail.com>
2012-12-28 00:06:25 +0800

    rm index.js for gh-pages

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello World, fnsoxt');
-}).listen(10080);
-console.log('Server is running.by fnsoxt');
